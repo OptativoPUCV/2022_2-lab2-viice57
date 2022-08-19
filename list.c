d03fb7f7254d7f3a8c9d0d684ebe97476e7c2492
Replit user <>
2022-08-19 13:06:41 +0000

    new try

--- list.c
+++ list.c
@@ -46,6 +46,7 @@ void * firstList(List * list) {
 
 /*   * La segunda función retorna el dato del nodo a continuación del current y actualiza el current para que apunte a ese nodo. */
 void * nextList(List * list) {
+  if(!list->current || !list->current->next) return NULL;
   list->current = list->current->next;
   return list->current->data;
 }
