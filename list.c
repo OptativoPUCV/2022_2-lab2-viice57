b2f2e9570e26997e6c88723c21161e1ca370d7e6
Replit user <>
2022-08-19 13:00:05 +0000

    new try

--- list.c
+++ list.c
@@ -49,7 +49,7 @@ void * nextList(List * list) {
   if(!list->current) return NULL;
   
   //Node * x = list->current;
-  list->current = list->next;
+  list->current = list->current->next;
   return list->current->data;
 }
 
